8031802d2bff9c8849912936467d9a5798362fc8
split-dev <asamusenko@splitdev.pro>
2020-04-10 11:36:28 +0300

    fix

--- resources/assets/scripts/util/polyfills.js
+++ resources/assets/scripts/util/polyfills.js
@@ -87,3 +87,15 @@ if ('NodeList' in window && !NodeList.prototype.forEach) {
     }
   };
 }
+// includes
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+
+    if (search instanceof RegExp) {
+      throw TypeError('first argument must not be a RegExp');
+    }
+    if (start === undefined) { start = 0; }
+    return this.indexOf(search, start) !== -1;
+  };
+}
