cfbed1478212df3e6267d87bf9538533a6db0410
split-dev <asamusenko@splitdev.pro>
2020-04-13 15:51:29 +0300

    child finish

--- resources/assets/scripts/routes/child.js
+++ resources/assets/scripts/routes/child.js
@@ -67,41 +67,22 @@ export default {
       var myDatepicker = $(element).datepicker().data('datepicker');
       myDatepicker.show();
     });
-    //size load
-    // eslint-disable-next-line no-unused-vars
-    $('.img-box label').click(function(){
-      let fileName = $(this).val().split('\\').pop();
-      let element = $(this).parent();
-      console.log(element);
-      element.text(fileName);
-      $('.file-load').fadeIn();
-      alert('top')
-    });
-    //remove
-    $('.file-load .remove').click( function (e) {
-      e.preventDefault();
-      $('.file-load').fadeOut();
-      $('.custom-file-input').val('');
-    });
     //add load button
     let number = 2;
     $('.add__button').click( function (e) {
       e.preventDefault();
-      console.log($('.img-box input'))
-      $('.img-box').append('<div class="form__img">\n' +
-        '            <input type="file" class="custom-file-input" id="customFile' + number + '">\n' +
-        '            <label for="customFile' + number + '"> <img src="images/icons/download.svg"> <img src="images/icons/white-download.svg"> Choose a file…</label>\n' +
-        '            <div class="file-load">\n' +
-        '              <span class="file-name"></span>\n' +
-        '              <a href="#" class="remove">Remove file</a>\n' +
-        '            </div>\n' +
-        '          </div>');
-      number = number + 1;
+      if (number <= 3) {
+        $('.img-box').append('<div class="form__img">\n' +
+          '            <input type="file" class="custom-file-input" onchange="element(this)" id="customFile' + number + '">\n' +
+          '            <label for="customFile' + number + '"> <img src="images/icons/download.svg"> <img src="images/icons/white-download.svg"> Choose a file…</label>\n' +
+          '            <div class="file-load">\n' +
+          '              <span class="file-name"></span>\n' +
+          '              <span data-file="customFile' + number + '" onclick="remove(this)" class="remove">Remove file</span>\n' +
+          '            </div>\n' +
+          '          </div>');
+        number = number + 1;
+      }
     });
-    // eslint-disable-next-line no-unused-vars
-    function element() {
-      console.log(333)
-    }
   },
   // JavaScript to be fired on all pages, after page specific JS is fired
   finalize() {
